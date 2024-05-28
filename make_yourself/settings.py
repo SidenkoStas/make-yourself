b8bc8c72b5ad09c00f0363fc943cbf2de8e242d6
SidenkoStas <volondss@gmail.com>
2024-05-28 19:52:37 +0300

    Продолжение разработки главной страницы сайта. Добавление css для базового шаблона

--- make_yourself/settings.py
+++ make_yourself/settings.py
@@ -147,6 +147,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "common/static"),
+    # os.path.join(BASE_DIR, "users/static"),
+    # os.path.join(BASE_DIR, "skill_tests/static"),
+    os.path.join(BASE_DIR, "blog/static"),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
